a7ff450534a85b89fdaaf19056874c9f3bb1c00e
Dawson Par <dawson@andrews.edu>
2023-07-17 20:11:45 -0500

    updated styling to max device width

--- src/components/info/Info02.tsx
+++ src/components/info/Info02.tsx
@@ -5,7 +5,9 @@ export const Info02 = () => {
   const matches = useMediaQuery("(min-width:450px)");
 
   return (
-    <div className={styles.info}>
+    <div
+      className={`${styles.info} ${matches ? styles.desktop : styles.mobile}`}
+    >
       <h2>Challenge 2 of 5: Welcome! 👋</h2>
       <h4>How to use useMediaQuery Component in ReactJS?</h4>
       <span>{`Is Screen at Minimum 450px: ${matches}`}</span>
