6ba25e805298dc124e52904fd7498fce5a739c50
Dawson Par <dawson@andrews.edu>
2023-07-27 16:22:17 -0500

    updated styles in info2

--- src/components/info/Info02.tsx
+++ src/components/info/Info02.tsx
@@ -2,7 +2,7 @@ import styles from "./info.module.css";
 
 export const Info02 = () => {
   return (
-    <div>
+    <div className={styles.info}>
       <h2>Challenge 2 of 5: Welcome! 👋</h2>
       <h4>How to use useMediaQuery Component in ReactJS?</h4>
     </div>
