3a7cae7d79da1285f737132a7c2a3d447dc4b98a
Dawson Par <dawson@andrews.edu>
2023-07-27 16:19:39 -0500

    updated info2

--- src/components/info/Info02.tsx
+++ src/components/info/Info02.tsx
@@ -1,16 +1,10 @@
 import styles from "./info.module.css";
-import useMediaQuery from "@mui/material/useMediaQuery";
 
 export const Info02 = () => {
-  const matches = useMediaQuery("(min-width:450px)");
-
   return (
-    <div
-      className={`${styles.info} ${matches ? styles.desktop : styles.mobile}`}
-    >
+    <div>
       <h2>Challenge 2 of 5: Welcome! 👋</h2>
       <h4>How to use useMediaQuery Component in ReactJS?</h4>
-      <span>{`Is Screen at Minimum 450px: ${matches}`}</span>
     </div>
   );
 };
